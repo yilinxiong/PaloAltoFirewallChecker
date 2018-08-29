a6024021872944e0729af64ca8bb542c5a2e9bbb
Mr X <mr.dreamer.xiong@hotmail.com>
2018-08-29 11:38:06 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+host = "PALOALTO-FIREWALL-HOST"
+api_username = "YOUR-API-USERNAME"
+api_password = "YOUR-API-PASSWORD"
+
+shared_vsys = "shared"
+vsys = "vsys1"
+fw_serial = "YOUR-FIREWALL-SERIAL-NUM"
